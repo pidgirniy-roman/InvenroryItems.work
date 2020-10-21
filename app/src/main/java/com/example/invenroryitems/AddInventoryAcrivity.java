9670c6d5fb87b70573971e7ed3bfea5db3b5e67f
Roman Pidgirniy <fen9.zp@gmail.com>
2020-10-21 14:43:24 +0300

    Finish

--- app/src/main/java/com/example/invenroryitems/AddInventoryAcrivity.java
+++ app/src/main/java/com/example/invenroryitems/AddInventoryAcrivity.java
@@ -1,28 +1,34 @@
 package com.example.invenroryitems;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
+import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.Toast;
-
-import com.example.invenroryitems.Data.InventoryNumberContract;
 
 import static com.example.invenroryitems.Data.InventoryNumberContract.InventoryEntry.*;
 
-public class AddInventoryAcrivity extends AppCompatActivity {
+public class AddInventoryAcrivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int EDIT_INVENTORY_LOADER = 111;
+    Uri currentInventoryUri;
 
     private EditText editTextDirectionOfWork;
     private EditText editTextTechniksInv;
@@ -35,9 +41,31 @@ public class AddInventoryAcrivity extends AppCompatActivity {
 
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+
+        if (currentInventoryUri == null){
+            MenuItem menuItem = menu.findItem(R.id.delete_inventory);
+            menuItem.setVisible(false);
+        }
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_inventory_acrivity);
+
+        Intent intent = getIntent();
+        currentInventoryUri = intent.getData();
+
+        if (currentInventoryUri == null) {
+            setTitle("Add a Inventory");
+            invalidateOptionsMenu();
+        } else {
+            setTitle("Edit the Inventory");
+            getSupportLoaderManager().initLoader(EDIT_INVENTORY_LOADER, null, this);
+        }
 
         editTextDirectionOfWork = findViewById(R.id.editTextDirectionOfWork);
         editTextTechniksInv = findViewById(R.id.editTextTechnicsInv);
@@ -85,6 +113,9 @@ public class AddInventoryAcrivity extends AppCompatActivity {
 //                directionOfWork = 0;
 //            }
 //        });
+
+
+
     }
 
     @Override
@@ -97,10 +128,11 @@ public class AddInventoryAcrivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.save_inventory:
-                insertInventory();
+                saveInventory();
                 return true;
 
             case R.id.delete_inventory:
+                showDeleteInventoryDialog();
                 return true;
 
             case android.R.id.home:
@@ -110,13 +142,24 @@ public class AddInventoryAcrivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void insertInventory() {
+    private void saveInventory() {
 
         String directionOfWork = editTextDirectionOfWork.getText().toString().trim();
         String technicsInv = editTextTechniksInv.getText().toString().trim();
         String status = editTextStatus.getText().toString().trim();
         String issusedBy = editTextIssuedBy.getText().toString().trim();
         String comment = editTextComment.getText().toString().trim();
+
+        if (TextUtils.isEmpty(directionOfWork)) {
+            Toast.makeText(this, "Введите ТТ/ЛО/ЦО-сотрудник", Toast.LENGTH_LONG).show();
+            return;
+        } else if (TextUtils.isEmpty(technicsInv)) {
+            Toast.makeText(this, "Введите ИНВ № Техники", Toast.LENGTH_LONG).show();
+            return;
+        } else if (TextUtils.isEmpty(issusedBy)) {
+            Toast.makeText(this, "Введите Дату выдачи", Toast.LENGTH_LONG).show();
+            return;
+        }
 
         ContentValues contentValues = new ContentValues();
         contentValues.put(COLUMN_DIRECTION_OF_WORK, directionOfWork);
@@ -125,12 +168,111 @@ public class AddInventoryAcrivity extends AppCompatActivity {
         contentValues.put(COLUMN_ISSUED_BY, issusedBy);
         contentValues.put(COLUMN_COMMENT, comment);
 
-        ContentResolver contentResolver = getContentResolver();
-        Uri uri = contentResolver.insert(CONTENT_URI, contentValues);
-        if (uri == null){
-            Toast.makeText(this, "Insertion of data in the table failed for", Toast.LENGTH_LONG).show();
-        }else {
-            Toast.makeText(this, "Data saved", Toast.LENGTH_LONG).show();
+        if (currentInventoryUri == null) {
+            ContentResolver contentResolver = getContentResolver();
+            Uri uri = contentResolver.insert(CONTENT_URI, contentValues);
+            if (uri == null) {
+                Toast.makeText(this, "Insertion of data in the table failed for",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Data saved", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            int rowsChange = getContentResolver().update(currentInventoryUri, contentValues, null, null);
+            if (rowsChange == 0) {
+                Toast.makeText(this,
+                        "Saving of data in the table failed", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,
+                        "Inventory updated", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        String[] prijection = {
+                _ID,
+                COLUMN_DIRECTION_OF_WORK,
+                COLUMN_TECHNICS_INV,
+                COLUMN_STATUS,
+                COLUMN_ISSUED_BY,
+                COLUMN_COMMENT
+        };
+        return new CursorLoader(
+                this,
+                currentInventoryUri,
+                prijection,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        if (cursor.moveToFirst()) {
+            int directionOfWorkColumnInvex = cursor.getColumnIndex(COLUMN_DIRECTION_OF_WORK);
+            int technicsInvColumnInvex = cursor.getColumnIndex(COLUMN_TECHNICS_INV);
+            int statusColumnInvex = cursor.getColumnIndex(COLUMN_STATUS);
+            int issuseByColumnInvex = cursor.getColumnIndex(COLUMN_ISSUED_BY);
+            int commentColumnInvex = cursor.getColumnIndex(COLUMN_COMMENT);
+
+            String directionOfWork = cursor.getString(directionOfWorkColumnInvex);
+            String technicsInv = cursor.getString(technicsInvColumnInvex);
+            String status = cursor.getString(statusColumnInvex);
+            String issuseBy = cursor.getString(issuseByColumnInvex);
+            String comment = cursor.getString(commentColumnInvex);
+
+            editTextDirectionOfWork.setText(directionOfWork);
+            editTextTechniksInv.setText(technicsInv);
+            editTextStatus.setText(status);
+            editTextIssuedBy.setText(issuseBy);
+            editTextComment.setText(comment);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+    }
+
+    private void showDeleteInventoryDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Вы действительно хотите удалить ");
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                deleteInventory();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (dialogInterface != null){
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void deleteInventory(){
+        if (currentInventoryUri != null){
+            int rowsDeleted = getContentResolver().delete(currentInventoryUri, null, null);
+
+            if (rowsDeleted == 0){
+                Toast.makeText(this,
+                        "Deleting of data from the table failed", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this,
+                        "Inventory is deleted", Toast.LENGTH_LONG).show();
+            }
+            finish();
         }
     }
 }
--- app/src/main/java/com/example/invenroryitems/MainActivity.java
+++ app/src/main/java/com/example/invenroryitems/MainActivity.java
@@ -7,10 +7,13 @@ import androidx.loader.app.LoaderManager;
 import androidx.loader.content.CursorLoader;
 import androidx.loader.content.Loader;
 
+import android.content.ContentUris;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -42,6 +45,17 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         inventoryCursorAdapter = new InventoryCursorAdapter(this, null, false);
         dataListView.setAdapter(inventoryCursorAdapter);
+
+        dataListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, AddInventoryAcrivity.class);
+                Uri currentInventoryUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id);
+                intent.setData(currentInventoryUri);
+                startActivity(intent);
+            }
+        });
+
         getSupportLoaderManager().initLoader(MEMBER_LOADER, null, this);
 
 
